9326133e7b7e709d47db80c955043e8608e88d2c
Alexandra Vlasova <Alex.Genrietta@gmail.com>
2020-09-17 21:59:30 +0300

    Made JS for aside part

---
+++ src-main/blocks/switcher/switcher.js
@@ -0,0 +1,46 @@
+const switcher = document.querySelector('.switcher');
+const switcherBtns = switcher.querySelectorAll('.switcher__item');
+console.log(switcherBtns);
+
+function hideElement (tag) {
+    const elem = document.querySelector(tag);
+    elem.classList.add('hide');
+}
+
+function showElement (tag) {
+    const elem = document.querySelector(tag);
+    elem.classList.remove('hide');
+}
+
+switcher.addEventListener('click', (event) => {
+    if ( !(event.target.classList.contains('switcher')) ) {
+        const target = event.target.closest('.switcher__item');
+
+        // Переключаем выделение на кнопках
+        switcherBtns.forEach( (elem, i) => {
+            if (elem.classList.contains('switch-checked')) {
+                if (elem != target) {
+                    elem.classList.remove('switch-checked');
+
+                    // Меняем содержание
+                    if (i === 0) {
+                        hideElement('.messages');
+                    } else {
+                        hideElement('.invites');
+                    }
+                }
+            } else {
+                if (elem == target) {
+                    elem.classList.add('switch-checked');
+
+                    // Меняем содержание
+                    if (i === 0) {
+                        showElement('.messages');
+                    } else {
+                        showElement('.invites');
+                    }
+                }
+            }
+        });
+    }
+});
--- src-main/js/index.js
+++ src-main/js/index.js
@@ -1,5 +1,8 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
 
+
 import '../blocks/photo-slider/sly.min.js';
 import '../blocks/photo-slider/slider.js';
+import '../blocks/person-card/person-card.js';
+import '../blocks/switcher/switcher.js';

f4fa96ddabf3fdaf2ced99181dbc6c7278dc69c7
Alexandra Vlasova <Alex.Genrietta@gmail.com>
2020-10-05 20:49:55 +0300

    Added slider in profile

---
+++ src-main/blocks/profile/profile.js
@@ -0,0 +1,16 @@
+const profile = document.querySelector('.aside-container');
+const closeProfileBtn = profile.querySelector('.close-btn');
+
+closeProfileBtn.addEventListener('click', () => {
+    profile.classList.add('hide');
+});
+
+const options = {
+    horizontal: false,
+    scrollBy: 100
+}
+
+const frame = ".profile-container";
+
+const sly = new Sly(frame, options);
+sly.init();
